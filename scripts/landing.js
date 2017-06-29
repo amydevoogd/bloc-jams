5e8b9c99568b9db6c07b31f73ddecfe3eb8488c0
Amy <amy@devoogd.com>
2017-06-29 17:21:52 -0500

    Assignment 21 DOMscripting Animation completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,4 @@
+/*
 var animatePoints = function() {
  
     var points = document.getElementsByClassName('point');
@@ -26,5 +27,26 @@ var animatePoints = function() {
         revealFirstPoint();
         revealSecondPoint();
         revealThirdPoint();
- 
     };
+        
+        animatePoints();
+*/
+
+
+var animatePoints = function() {
+ 
+    var points = document.getElementsByClassName('point');
+
+    var revealPoint = function() {
+        for (var i = 0; i < points.length; i++) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+    };
+        revealPoint();
+    };
+        animatePoints();
+
+
